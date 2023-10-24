95325e27f48d3db54e7c743b0ee6ab6f10155659
ajudson23 <judslabs@icloud.com>
2023-10-23 18:17:46 -0600

    modify settings & create app

--- django_project/settings.py
+++ django_project/settings.py
@@ -29,7 +29,6 @@ ALLOWED_HOSTS = []
 
 
 # Application definition
-
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -37,7 +36,15 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # Add your app name here
+    'hockey_app', 
 ]
+
+# Add support for authenticating users
+AUTHENTICATION_BACKENDS = [
+   'django.contrib.auth.backends.ModelBackend',
+]
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
