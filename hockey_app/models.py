94c62a66252a80be3b9dd4dae401cb86d46b7b1d
ajudson23 <judslabs@icloud.com>
2023-10-25 16:54:58 -0600

    added model

--- hockey_app/models.py
+++ hockey_app/models.py
@@ -1,17 +1,24 @@
 from django.db import models
 
 class Player(models.Model):
-    #List of choices for major value in database, human readable name
-    LEVELS = (
-        ('Advanced'),('Intermediate'),('N/A'),
-    )
-    TEAMNAME = (
-        ('Name1'),('Name2'),('Name3'),('Name4'),('Name5'),('Name6'),('Name7'),('N/A'),
-    )
-    firstName = models.CharField(max_length=200)
-    lastName = models.CharField(max_length=200)
-    email = models.CharField(max_length=200)
-    teamLevel = models.CharField(max_length=200, choices=LEVELS, blank = True)
-    teamName = models.CharField(max_length=200, choices=TEAMNAME, blank = True)
-    phoneNum = models.CharField(max_length=200)
-
+    LEVELS = [
+        ('Advanced', 'Advanced'),
+        ('Intermediate', 'Intermediate'),
+        ('N/A', 'N/A'),
+    ]
+    TEAMNAME = [
+        ('Name1', 'Name1'),
+        ('Name2', 'Name2'),
+        ('Name3', 'Name3'),
+        ('Name4', 'Name4'),
+        ('Name5', 'Name5'),
+        ('Name6', 'Name6'),
+        ('Name7', 'Name7'),
+        ('N/A', 'N/A'),
+    ]
+    firstName = models.CharField("First Name", max_length=200)
+    lastName = models.CharField("Last Name", max_length=200)
+    email = models.CharField("Email", max_length=200)
+    teamLevel = models.CharField("Team Level", max_length=200, choices=LEVELS, blank=False)
+    teamName = models.CharField("Team Name", max_length=200, choices=TEAMNAME, blank=False)
+    phoneNum = models.CharField("Phone Number", max_length=200)
