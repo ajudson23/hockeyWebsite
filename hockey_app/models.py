6ac71de6a80e40849aed8830b10e0c1a74a94371
ajudson23 <judslabs@icloud.com>
2023-10-25 16:00:21 -0600

    rid of pycache stuff

--- hockey_app/models.py
+++ hockey_app/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class Player(models.Model):
+    #List of choices for major value in database, human readable name
+    LEVELS = (
+        ('Advanced'),('Intermediate'),('N/A'),
+    )
+    TEAMNAME = (
+        ('Name1'),('Name2'),('Name3'),('Name4'),('Name5'),('Name6'),('Name7'),('N/A'),
+    )
+    firstName = models.CharField(max_length=200)
+    lastName = models.CharField(max_length=200)
+    email = models.CharField(max_length=200)
+    teamLevel = models.CharField(max_length=200, choices=LEVELS, blank = True)
+    teamName = models.CharField(max_length=200, choices=TEAMNAME, blank = True)
+    phoneNum = models.CharField(max_length=200)
+
